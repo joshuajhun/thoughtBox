42cb4df3eb5356a52fc82e0ad966f24797df1ab4
joshuajhun <joshuajhun@gmail.com>
2016-02-29 17:15:52 -0700

    added security measure. if user tries to acces the links page it willl redirect them to sign in and if they dont have an account there is a link to sign up on the page

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,4 +1,8 @@
 class LinksController < ApplicationController
-    def index
-    end
+  before_action :require_authenticated_user
+  def require_authenticated_user
+    redirect_to login_path flash[:login] ="Must Login" unless current_user
+  end
+  def index
+  end
 end
